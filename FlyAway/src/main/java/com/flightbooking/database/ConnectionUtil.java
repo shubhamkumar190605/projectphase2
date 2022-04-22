faf4d0e44ea0ba51ac5b40db0044ca998b0ceef5
Shubham Shrivastava <srivastava.sk987@gmail.com>
2022-04-22 17:56:02 +0530

    first commit

---
+++ FlyAway/src/main/java/com/flightbooking/database/ConnectionUtil.java
@@ -0,0 +1,12 @@
+package com.flightbooking.database;
+
+import java.util.List;
+
+public class ConnectionUtil {
+
+	public List<String[]> getAvailableFlights(String from, String to, String departure) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
